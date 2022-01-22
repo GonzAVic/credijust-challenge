b0f1f57b5c0d3add7a7f8b8d635e3731fbedd854
Victor <vic@yellowme.mx>
2022-01-22 17:24:32 -0600

    --------- This is all for today ---------

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+/**
+ * Test...
+ *  C O M I N G   S O O N
+ */
+
+test('Renders lofin form', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
