2877c73a1b1efb51b21bfd76b0412885ad1e6626
Victor <vic@yellowme.mx>
2022-01-23 13:49:30 -0600

    Update: Update arrows with last coin value

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -4,9 +4,18 @@ import React from "react";
 import Text from "./Text";
 
 const CoinTable = ({ name, history }) => {
+  const displayArrow = (isUp) => {
+    return isUp ? "⇧" : "⇩";
+  };
+
   return (
     <div data-tid={`coin-table-${name}`} className="coin-table">
-      <Text as="h2">123,456</Text>
+      <Text as="h2">
+        {history[0] &&
+          history[1] &&
+          displayArrow(history[0].value > history[1].value)}
+        {history[0].value}
+      </Text>
       <Text as="label">{name}</Text>
       <br />
       <div className="coin-table--history">
